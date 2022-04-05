d381dcd2e26b5f5e177d611423d09083628a9094
Ch Shiva KUmar <85950488+Shivakumar1090@users.noreply.github.com>
2022-04-05 16:09:29 +0530

    Time: 95 ms (46.17%), Space: 23.6 MB (91.48%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int countSquares(vector<vector<int>>& arr) {
         int rows = arr.size() , cols = arr[0].size();
-        int k = max(rows,cols)+1;
-        vector<int> sum(k,0);
         int res = 0;
         
         for(int i = 1; i < rows; i++){
             for(int j = 1; j < cols; j++){
                 if(arr[i][j] == 1){
-                    arr[i][j] = min(min(arr[i][j-1] , arr[i-1][j]) , arr[i-1][j-1])+1;
+                    arr[i][j] = min(min(arr[i][j-1] , arr[i-1][j]) , arr[i-1][j-1])+1;   
                 }
             }
         }
