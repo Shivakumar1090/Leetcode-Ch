19b22faa09712687776acf68905e68205dc0db65
Ch Shiva KUmar <85950488+Shivakumar1090@users.noreply.github.com>
2022-04-17 23:16:35 +0530

    Time: 11 ms (29.40%), Space: 16.5 MB (17.52%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -13,9 +13,9 @@ class Solution {
 public:
     bool mirror(TreeNode* root1 , TreeNode* root2){
         if(root1 == NULL && root2 == NULL)return true;
-        if(root1 == NULL || root2 == NULL)return false;
+        else if(root1 == NULL || root2 == NULL)return false;
         
-        if(root1->val != root2->val)return false;
+        else if(root1->val != root2->val)return false;
         
         return mirror(root1->left,root2->right) && mirror(root1->right,root2->left);
     }
