ce75839f48b514db39f7ec36dd44db7e7a915cf0
Ch Shiva KUmar <85950488+Shivakumar1090@users.noreply.github.com>
2022-04-19 00:45:26 +0530

    Time: 65 ms (83.80%), Space: 28.7 MB (83.94%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s;
+        s.insert(nums.begin() , nums.end());
+        int res = 0;
+        
+        for(auto it: s){
+            if(s.count(it-1))continue;
+            int j = 1;
+            while(s.count(it+j)){
+                j++;
+            }
+            res = max(res,j);
+        }
+        return res;
+    }
+};
