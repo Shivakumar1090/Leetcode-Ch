3b2a1714dabf15c74ebc9da2bf78b376c364f786
Ch Shiva KUmar <85950488+Shivakumar1090@users.noreply.github.com>
2022-06-03 13:01:24 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (85.69%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // Intial     Middle    Final
+    // 0            -1         1
+    // 1            -2         0
+    
     int dx[8] = {0,0,1,-1,1,-1,1,-1};
     int dy[8] = {1,-1,0,0,1,1,-1,-1};
         
@@ -13,7 +17,7 @@ public:
         }
         
         if(count == 3){
-            board[i][j] = -1; // -1 == 1
+            board[i][j] = -1; 
         }
     }
     
@@ -22,19 +26,16 @@ public:
         
         for(int k = 0; k < 8; k++){
             int x = i+dx[k] , y = j+dy[k];
-            
             if(x < 0 || y < 0 || x >= n || y >= m)continue;
-            
             if(board[x][y] == 1 || board[x][y] == -2)count++;
         }
         if(count > 3 || count < 2){
-            board[i][j] = -2; // -2 == 0
+            board[i][j] = -2;
         }
         
     }
     void gameOfLife(vector<vector<int>>& board) {
         int n = board.size() , m = board[0].size();
-        // vector<vector<int>> c = board;
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
