9c45e3c252f11525b83a0fe20f1f1495bc54fcee
Ch Shiva KUmar <85950488+Shivakumar1090@users.noreply.github.com>
2022-06-03 12:59:57 +0530

    Time: 6 ms (15.87%), Space: 6.9 MB (85.69%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -3,49 +3,54 @@ public:
     int dx[8] = {0,0,1,-1,1,-1,1,-1};
     int dy[8] = {1,-1,0,0,1,1,-1,-1};
         
-    void check0(int i , int j , int n , int m ,vector<vector<int>> &board , vector<vector<int>>&c){
+    void check0(int i , int j , int n , int m ,vector<vector<int>> &board){
         int count = 0;
         
         for(int k = 0; k < 8; k++){
             int x = i+dx[k] , y = j+dy[k];
             if(x < 0 || y < 0 || x >= n || y >= m)continue;
-            if(c[x][y] == 1)count++;
+            if(board[x][y] == 1 || board[x][y] == -2)count++;
         }
         
         if(count == 3){
-            board[i][j] = 1;
+            board[i][j] = -1; // -1 == 1
         }
     }
     
-    void check1(int i , int j , int n , int m , vector<vector<int>> &board , vector<vector<int>>& c){
-        int count0 = 0, count1 = 0;
+    void check1(int i , int j , int n , int m , vector<vector<int>> &board){
+        int count = 0;
         
         for(int k = 0; k < 8; k++){
             int x = i+dx[k] , y = j+dy[k];
             
             if(x < 0 || y < 0 || x >= n || y >= m)continue;
             
-            if(c[x][y] == 1)count1++;
+            if(board[x][y] == 1 || board[x][y] == -2)count++;
         }
-        if(count1 > 3 || count1 < 2){
-            board[i][j] = 0;
+        if(count > 3 || count < 2){
+            board[i][j] = -2; // -2 == 0
         }
         
     }
     void gameOfLife(vector<vector<int>>& board) {
         int n = board.size() , m = board[0].size();
-        vector<vector<int>> c = board;
+        // vector<vector<int>> c = board;
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(board[i][j] == 0){
-                    check0(i,j,n,m,board,c);
+                if(board[i][j] == 0 || board[i][j] == -1){
+                    check0(i,j,n,m,board);
                 } else {
-                    check1(i,j,n,m,board,c);
+                    check1(i,j,n,m,board);
                 }
             }
         }
         
-        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == -2)board[i][j] = 0;
+                if(board[i][j] == -1)board[i][j] = 1;
+            }
+        }
     };
 };
