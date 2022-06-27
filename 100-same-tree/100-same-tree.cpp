33e42e9d2c7f9712942353c876eb24cee5b68d8a
Ch Shiva KUmar <85950488+Shivakumar1090@users.noreply.github.com>
2022-06-27 14:35:08 +0530

    Time: 3 ms (66.20%), Space: 9.8 MB (95.01%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -15,13 +15,6 @@ public:
         if(p == NULL && q == NULL)return true;
         if(p == NULL || q == NULL)return false;
         if(p->val != q->val)return false;
-        
-        // if(p->left && !q->left)return false;
-        // if(p->right && !q->right)return false;
-        // if(!p->left && q->left)return false;
-        // if(!p->right && q->right)return false;
-        // if(p->left && p->right && q->left && q->right)
-        //     if((p->left->val != q->left->val) || (p->right->val != q->right->val) )return false;
         return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
     }
 };
