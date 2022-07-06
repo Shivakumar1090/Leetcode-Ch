b0b0472b310ddf39accc27794331f79300b4d692
CH Shiva Kumar <85950488+Shivakumar1090@users.noreply.github.com>
2022-07-06 16:09:55 +0530

    Time: 37 ms (26.95%), Space: 9 MB (56.63%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -20,9 +20,32 @@ public:
     int minDistance(string word1, string word2) {
         int l1 = word1.length() , l2 = word2.length();
         
-        vector<vector<int>> memo(l1+1,vector<int>(l2+1,-1));
+        vector<vector<int>> dp(l1+1,vector<int>(l2+1,-1));
         
-        int l = lcs(word1,word2,l1 ,l2 , memo);
-        return l;
+        for(int i = 0; i < l1+1; i++){
+            dp[i][0] = i;
+        }
+        
+        for(int i = 0; i < l2+1; i++){
+            dp[0][i] = i;
+        }
+        
+        dp[0][0] = 0;
+        
+        for(int i = 1; i < l1+1; i++){
+            for(int j = 1; j < l2+1; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int insertChar = dp[i][j-1];
+                    int deleteChar = dp[i-1][j];
+                    int replaceChar = dp[i-1][j-1];
+                    dp[i][j] = 1+min({insertChar,deleteChar,replaceChar});
+                }
+            }
+        }
+        
+        return dp[l1][l2];
     }
 };
